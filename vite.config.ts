c896c1e2aa200fede2ed0b8abe1697947dbd3085
semlex <picasso.semenov.spb@gmail.com>
2025-04-26 22:12:16 +0300

    gh-pages config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/crush-the-mole',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
